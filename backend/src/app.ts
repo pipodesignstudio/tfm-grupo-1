2ccbd1516ed9cdf874ad73473cd8817278c14a9a
Pipo_Studio <pipodesignstudio@gmail.com>
2025-05-22 10:10:41 +0200

    20250522: Añadido soporte nodemon y cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import { corsConfig } from './config';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors(corsConfig))
 
 app.get('/', (_req, res) => {
   res.send('Hola mundo');
