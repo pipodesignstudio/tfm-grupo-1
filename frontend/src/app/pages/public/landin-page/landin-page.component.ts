efae7ee61aaff4aa2dcd27f72e7d75255442592e
Ángela <angeladonoso12@gmail.com>
2025-05-28 09:49:20 +0200

    Creación de componentes frontend

---
+++ frontend/src/app/pages/public/landin-page/landin-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landin-page',
+  imports: [],
+  templateUrl: './landin-page.component.html',
+  styleUrl: './landin-page.component.css'
+})
+export class LandinPageComponent {
+
+}
---
+++ frontend/src/app/pages/routines/calendar-page/calendar-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-page',
+  imports: [],
+  templateUrl: './calendar-page.component.html',
+  styleUrl: './calendar-page.component.css'
+})
+export class CalendarPageComponent {
+
+}
