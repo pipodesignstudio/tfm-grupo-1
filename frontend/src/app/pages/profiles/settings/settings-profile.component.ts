9cef1e5bb204ba6dd9f45a61c581dec2472154fe
Gorosss <jongorostegui@gmail.com>
2025-06-05 18:23:53 +0200

    settings-profile page created

---
+++ frontend/src/app/pages/profiles/settings/settings-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  imports: [],
+  templateUrl: './settings-profile.component.html',
+  styleUrl: './settings-profile.component.css'
+})
+export class SettingsComponent {
+
+}
