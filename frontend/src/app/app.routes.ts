e29197d93e193673b316b7007dc8c295c05726bc
Ángela <angeladonoso12@gmail.com>
2025-06-10 18:33:32 +0200

    Implantación y animaciones log in
    Diseño de la página de log in con un pequeño validador

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,22 @@
-import { Routes } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { LoginPageComponent } from './pages/auth/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/auth/register-page/register-page.component';
 import { CalendarPageComponent } from './pages/routines/calendar-page/calendar-page.component';
 import { WelcomePageComponent } from './public/welcome-page/welcome-page.component';
+import { DashboardPageComponent } from './pages/dashboard/dashboard-page/dashboard-page.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomePageComponent, title: 'Bienvenido a Nido' },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
   { path: 'calendar', component: CalendarPageComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- frontend/src/app/pages/auth/login-page/login-page.component.ts
+++ frontend/src/app/pages/auth/login-page/login-page.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogin(): void {
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Por favor, introduce tu mail y contraseña';
+      return;
+    }
+
+    this.router.navigate(['/dashboard']);
+  }
+
+  goToRegister(): void {
+    this.router.navigate(['/register']);
+  }
 }
