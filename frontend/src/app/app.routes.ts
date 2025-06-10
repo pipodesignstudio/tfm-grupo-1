2a6b94c4b77b7a91424131d7fb1c4c0d7455275c
Ángela <angeladonoso12@gmail.com>
2025-06-10 18:04:13 +0200

    Implementación y animaciones del Welcome
    - Crea el componente WelcomePageComponent con contenido inicial.
    - Aplica estilos con paleta de colores y fuente Poppins.
    - Añade animación de fade-in/slide-up al contenido.
    - Implementa animación de hover para el botón
    - Agrega animación de subrayado debajo del título.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 
-import { LandinPageComponent } from './pages/public/landin-page/landin-page.component';
+import { LandinPageComponent } from './public/landin-page/landin-page.component';
 import { LoginPageComponent } from './pages/auth/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/auth/register-page/register-page.component';
 import { CalendarPageComponent } from './pages/routines/calendar-page/calendar-page.component';
+import { WelcomePageComponent } from './public/welcome-page/welcome-page.component';
 
 export const routes: Routes = [
-    {path: "", pathMatch: "full", component: LandinPageComponent},
-    {path: "login", component: LoginPageComponent},
-    {path: "register", component: RegisterPageComponent},
-    {path: "calendar", component: CalendarPageComponent},
-    {path: "**", redirectTo: "", pathMatch: "full"}
-];;
+  { path: '', component: WelcomePageComponent, title: 'Bienvenido a Nido' },
+  { path: 'home', component: LandinPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'calendar', component: CalendarPageComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
