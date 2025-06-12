cd1a9b524402678b3e192aeabd0e5e55b0387ebd
Gorosss <jongorostegui@gmail.com>
2025-06-12 21:00:27 +0200

    user profile mostly done

--- frontend/src/app/pages/profiles/user-profile/user-profile.component.ts
+++ frontend/src/app/pages/profiles/user-profile/user-profile.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [AvatarModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
 
+constructor(public router: Router) {}
+
+  children = [
+    { name: 'Emilio', value: '1', selected: true },
+    { name: 'Ramon', value: '2' },
+  ];
+
+  user = {
+    nick: 'Maxito',
+    name: 'Max',
+    surname: 'Mustermann',
+    avatar: 'https://gravatar.com/avatar/7156d0abbfcc494cf10e4adab6a8884d?s=400&d=robohash&r=x',
+    rol: 'admin',
+  };
 }
