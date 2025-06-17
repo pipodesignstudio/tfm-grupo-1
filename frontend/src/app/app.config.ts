5d61887727381bff0170b70f1fca12457df37ba4
Gorosss <jongorostegui@gmail.com>
2025-06-17 19:23:27 +0200

    caledar service working

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +23,7 @@ export const appConfig: ApplicationConfig = {
         }
         }
       }
-    })
+    }),
+    provideHttpClient()
   ]
 };
