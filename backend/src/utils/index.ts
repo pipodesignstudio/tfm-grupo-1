e15126ef17331e2cf169203da3b7240cb2dc9aae
Pipo_Studio <pipodesignstudio@gmail.com>
2025-06-23 11:29:25 +0200

    20250624 | Merge Esther con ppal

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './async-middleware.util';
 export * from './errors.util';
 export * from './jwt.util';
+export * from './lista-alergias';
 export * from './logger.util';
 export * from './pdf-definitions.util';
 export * from './success-api-response.util';
