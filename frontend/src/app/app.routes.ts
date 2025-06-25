b79b5da7d21fe21eda1fab3720f8fcc499700ed9
Ángela <angeladonoso12@gmail.com>
2025-06-25 20:55:01 +0200

    Creacion de estilos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -78,8 +78,8 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardLayoutComponent,
-    canActivate: [authGuard],
-    canActivateChild: [dashboardGuard],
+    // canActivate: [authGuard],
+    // canActivateChild: [dashboardGuard],
     children: [
       {
         path: 'create-family',
